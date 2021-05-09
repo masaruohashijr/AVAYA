1afa3a5461341300bb7e1e283c51a49361f24426
Masaru Ohashi Junior <masaru.ohashi@gmail.com>
2021-05-09 17:18:00 -0300

    testing SUCCESS

--- Top10CallAtendants_test.go
+++ Top10CallAtendants_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	mdl "AVAYA/models"
 	"fmt"
+	"regexp"
 	"testing"
 )
 
@@ -10,12 +11,17 @@ func TestSort(t *testing.T) {
 	var att mdl.Attendant
 	var atts []mdl.Attendant
 	att.Name = "1"
-	att.TotalMonthCalls = 10
+	att.TotalMonthCalls = 20
 	atts = append(atts, att)
 	att = mdl.Attendant{}
 	att.Name = "2"
-	att.TotalMonthCalls = 5
+	att.TotalMonthCalls = 8
 	atts = append(atts, att)
-	res, _ := Sort(atts)
-	fmt.Print(res)
+
+	want := regexp.MustCompile("\\**(20 calls){1}\\.*")
+	fmt.Print(want)
+	res, err := Sort(atts)
+	if !want.MatchString(res) || err != nil {
+		t.Fatal("Erro", res, err, want)
+	}
 }
